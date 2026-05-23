b08e92895889844cbea1326c886ab9f1f693c06e
Oskar Kogut <kogutoskar123@gmail.com>
2026-05-23 12:12:53 +0200

    HCCore and SLD3_Image

--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,52 @@ workspace "HC"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+project "HCCore"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+	{
+        "src/HCCore/src/**.h",
+        "src/HCCore/src/**.hpp",
+        "src/HCCore/src/**.cpp",
+		"src/HCCore/include/**.h",
+        "src/HCCore/include/**.hpp",
+    }
+
+    includedirs 
+	{
+        "vendor/SDL3/include",
+        "vendor/SDL3_image/include"
+    }
+
+    libdirs 
+	{
+        "vendor/SDL3/lib/x64",
+        "vendor/SDL3_image/lib/x64"
+    }
+
+    links 
+	{
+        "SDL3",
+        "SDL3_image",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        symbols "On"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        optimize "On"
+        runtime "Release"
+
 project "HCGame"
     kind "ConsoleApp"
     language "C++"
@@ -13,25 +59,36 @@ project "HCGame"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "src/**.h",
-        "src/**.hpp",
-        "src/**.cpp"
+    files 
+	{
+        "src/HCGame/src/**.h",
+        "src/HCGame/src/**.hpp",
+        "src/HCGame/src/**.cpp",
+		"src/HCGame/include/**.h",
+        "src/HCGame/include/**.hpp",
     }
 
-    includedirs {
-        "vendor/SDL3/include"
+    includedirs 
+	{
+        "vendor/SDL3/include",
+        "vendor/SDL3_image/include",
+		"src/HCCore/include",
     }
 
-    libdirs {
-        "vendor/SDL3/lib/x64"
+    libdirs 
+	{
+        "vendor/SDL3/lib/x64",
+        "vendor/SDL3_image/lib/x64"
     }
 
-    links {
-        "SDL3"
+    links 
+	{
+        "SDL3",
+        "SDL3_image",
     }
 
-    postbuildcommands {
+    postbuildcommands 
+	{
          '{COPYFILE} "%{wks.location}/vendor/SDL3/lib/x64/SDL3.dll" "%{cfg.targetdir}"'
     }
 
---
+++ src/HCGame/src/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <SDL3/SDL.h>
+#include "SDL3_image/SDL_image.h"
+#include "HCCore/Core/Types.h"
+
+#include <iostream>
+
+struct SDLState
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+void Cleanup(SDLState& state);
+
+int main()
+{
+    SDLState state;
+    
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "SDL could not initialize", nullptr);
+        return -1;
+    }
+    
+    i32 width = 800;
+    i32 height = 600;
+    state.window = SDL_CreateWindow("HCGame", width, height, 0);
+    
+    if (!state.window)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", state.window);
+        Cleanup(state);
+        return -1;
+    }
+    
+    state.renderer = SDL_CreateRenderer(state.window, nullptr);
+    if (!state.renderer)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating renderer", state.window);
+        Cleanup(state);
+        return -1;
+    }
+    
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event {0};
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_EVENT_QUIT:
+                {
+                    running = false;
+                    break;
+                }
+            }
+        }
+        
+        SDL_SetRenderDrawColor(state.renderer, 123, 21, 67, 255);
+        SDL_RenderClear(state.renderer);
+        
+        SDL_RenderPresent(state.renderer);
+    }
+    
+    Cleanup(state);
+
+    return 0;
+}
+
+void Cleanup(SDLState& state)
+{
+    SDL_DestroyRenderer(state.renderer);
+    SDL_DestroyWindow(state.window);
+    SDL_Quit();
+}
